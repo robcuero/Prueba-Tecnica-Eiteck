e816ee6991640d18d45c9559ed79b566fb3ebb74
Jorge Loayza <jloayzaz@pichincha.com>
2022-07-01 21:46:29 -0500

    ready

--- src/store/api/reservationApi.ts
+++ src/store/api/reservationApi.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const test = axios.create({
 	baseURL: "http://localhost:3001",
+	// baseURL: "https://my-json-server.typicode.com/robcuero/Prueba-Tecnica-Eiteck",
 })
 export default test
 
